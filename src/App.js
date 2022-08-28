15af4d4848ffe07b4257754179862256fdafa352
Mozgur <dfaunm@gmail.com>
2022-08-28 15:55:12 +0300

    Changed browser router to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav";
 import About from "./Components/About";
 import Footer from "./Components/Footer";
